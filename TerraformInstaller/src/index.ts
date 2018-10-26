7ab74ff5150b0455b53c418c234ea661b2d24bf9
Charles Zipp <charleszipp@gmail.com>
2018-10-26 10:55:06 -0500

    initial commit

---
+++ TerraformInstaller/src/index.ts
@@ -0,0 +1,30 @@
+import tasks = require('vsts-task-lib/task');
+import * as installer from './installer'
+import * as tools from 'vsts-task-tool-lib/tool';
+import * as path from 'path';
+
+async function configureTerraform(){
+    var inputVersion = tasks.getInput("terraformVersion", true);
+    var terraformPath = await installer.download(inputVersion);
+    var envPath = process.env['PATH'];
+    if(envPath && !envPath.startsWith(path.dirname(terraformPath))){
+        tools.prependPath(path.dirname(terraformPath));
+    }
+}
+
+async function verifyTerraform(){
+    console.log("Verifying Terraform installation. Executing 'terraform version'");
+    var terraformToolPath = tasks.which("terraform", true);
+    var terraformTool = tasks.tool(terraformToolPath);
+    terraformTool.arg("version");
+    return terraformTool.exec()
+}
+
+configureTerraform()
+    .then(() => verifyTerraform())
+    .then(() => {
+        tasks.setResult(tasks.TaskResult.Succeeded, "");
+    })
+    .catch((error) => {
+        tasks.setResult(tasks.TaskResult.Failed, error)
+    })
