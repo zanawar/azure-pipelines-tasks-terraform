3413bfad4bfcf4e1a6290191f2fb2a0b6fc7c991
Charles Zipp <chzipp@microsoft.com>
2020-02-26 14:29:29 -0600

    fixed sample terraform templates to be compatible with azurerm 2.0.0 (#113)
    * fixed sample terraform templates to be compatible with azurerm 2.0.0 version
    
    * ran terraform 0.12upgrade

--- TerraformTemplates/sample/sample.tf
+++ TerraformTemplates/sample/sample.tf
@@ -1,4 +1,8 @@
 resource "azurerm_resource_group" "rg" {
-    name = "${join("-", list("rg", var.app-short-name, var.env-short-name, var.region))}"
-    location = "${var.region}"
+  name = join(
+    "-",
+    ["rg", var.app-short-name, var.env-short-name, var.region],
+  )
+  location = var.region
 }
+
